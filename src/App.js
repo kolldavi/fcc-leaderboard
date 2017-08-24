69c909731f3ac4f02e9967ea855c1f780af3079f
kolldavi <kolldavi@gmail.com>
2017-08-24 09:38:06 -0500

    edit App.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,52 @@
 import React, { Component } from 'react';
 import logo from './freecodecamp_logo.svg';
-import User from './User'
+import User from './User';
 import './App.css';
 
 class App extends Component {
-
   state = {
-    results:[]
-  }
+    results: []
+  };
 
   getLeaderBoardRecent = () =>
-    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent',
-      {'Accept': 'application/json'})
+    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent', {
+      Accept: 'application/json'
+    })
       .then(response => response.json())
-      .then(data => data)
+      .then(data => data);
 
   getLeaderBoardAllTime = () =>
-  fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime',
-    {'Accept': 'application/json'})
-    .then(response => response.json())
-    .then(data => data)
+    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime', {
+      Accept: 'application/json'
+    })
+      .then(response => response.json())
+      .then(data => data);
 
-
-componentDidMount(){
-  this.getLeaderBoardRecent().then(results =>{
-    this.setState({results})
-  })
-}
-
+  componentDidMount() {
+    this.getLeaderBoardRecent().then(results => {
+      this.setState({ results });
+    });
+  }
   render() {
-    const results = this.state.results
+    const results = this.state.results;
     return (
       <div className="App">
         <div className="header">
-          <a href='https://www.freecodecamp.org/'><img src={logo} alt='freecodecamp link' className='logo'/></a>
+          <a href="https://www.freecodecamp.org/">
+            <img src={logo} alt="freecodecamp link" className="logo" />
+          </a>
         </div>
         <div>
-          <div className='header leaderboard-header'>
+          <div className="header leaderboard-header">
             <h2>Leaders</h2>
-          <ol>
-          {results.map(user =>(
-            <li key={user.username}>
-            <User user={user}/>
-            </li>
-          ))}
-        </ol>
-        </div>
+            <ol>
+              {results.map(user =>
+                <li key={user.username}>
+                  <User user={user} />
+                </li>
+              )}
+            </ol>
+          </div>
         </div>
       </div>
     );
